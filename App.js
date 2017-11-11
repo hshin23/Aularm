39485f37d20fd075cb7a4a8b0abe0fd642d4d544
Joe Brock <jbrock2@masonlive.gmu.edu>
2017-11-10 20:36:47 -0500

    This is an attempt to place a stackrouter

--- App.js
+++ App.js
@@ -13,24 +13,77 @@ import {
   View
 } from 'react-native';
 
-import { StackNavigator, } from 'react-navigation';
+import { StackRouter, } from 'react-navigation';
 // if you want to add more react native screens by importing them directly
 
+
+// This represents the possible routes that we could take
+const route = {
+  //Defines Navigation state in response to an action
+  getStateForAction: (action,state) => ({
+    index: 1,
+    routes: [
+      {
+        routeName: 'startRoute',
+        key: 'start',
+      },
+      {
+        routeName: 'questionRoute',
+        key: 'question',
+      },
+      {
+        routeName: 'clockRoute',
+        key: 'clock',
+      },
+    ]
+  }),
+  //Returns the child component or navigator for the given route name
+  getActionForPathAndParams: (path, params) => null,
+  //Returns the active component for a deep navigation state
+  getPathAndParamsForState: (state) => null,
+  //Returns an optional navigation action that should be used when
+  //The user navigates to this path and provides optional query params
+  getComponentForState: (state) => Navi,
+  //Returns the path and params that can be put into the URL to link the user
+  //back to the same spot
+  getComponentForRouteName:(routeName) => Navi,
+};
+
+class NaviGuide extends React.Component {
+
+/*  static Navi = StackRouter({
+    Home: {screen: App},
+    Question: {screen: Questionnaire},
+    }, {
+    initialRouteName: 'Home',
+  });*/
+  // "Navi" hosts the route list, so that way we can work on it
+  static Navi = route; 
+  //static defaultGetStateForAction = Navi.router.getStateForAction;
+}
+
+//This is going to represent the next screen that we move to
 class Questionnaire extends React.Component {
   render() {
     return(
-
+      <Text>Ye</Text>
     );
   }  
 }
 
 export default class App extends Component<{}> {
+  //This is for determining what path we go to
+
+  moveTo() {
+    
+  }
+
   render() {
     return (
       <View style={{flex: 1, padding: 60, backgroundColor: 'grey'}}>
 	<Text style={styles.welcome}>Welcome to A4DN</Text>
         <Text style={{textAlign:'center'}}>The alarm system based around you!</Text>
-	<Button onPress={this._moveToTest} title="Get Started"/>
+	<Button onPress={this._moveTo} title="Get Started"/>
       </View>
     );
   }
@@ -43,10 +96,3 @@ const styles = StyleSheet.create({
     margin: 20,
   }
 });
-
-const Navi = StackNavigator({
-  Home: {screen: App},
-  Question: {screen: Questionnaire},
-});
-
-
