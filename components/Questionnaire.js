8fa64298dac8919cabccf04746f345fee03cbde6
Joe Brock <jbrock2@masonlive.gmu.edu>
2017-11-14 08:56:40 -0500

    Prev solution didn't work

---
+++ components/Questionnaire.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View 
+} from 'react-native';
+
+import { StackRouter, } from 'react-navigation';
+
+class Questionnaire extends React.Component {
+  render() {
+    const {navigate} = this.props.navigation;
+
+    return(
+      <Text>Ye</Text>
+    );
+  }
+}
+//export default Questionnaire;
